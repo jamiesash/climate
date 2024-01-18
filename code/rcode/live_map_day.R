5d7e3d3ef9fa9a27831a71db221f3befa6a0487a
jamiesash <jamesash@hawii.edu>
2024-01-18 02:48:58 +0000

    :)

--- code/rcode/live_map_day.R
+++ code/rcode/live_map_day.R
@@ -1,64 +1,67 @@
+
 # ------------------------------------------------------------------------------
-### Liraries and functions
+### Liraries 
 library(cmocean)
 library(rworldmap)
 library(rworldxtra)
-library(anytime)
 library(terra)
+library(anytime)
 
 print("2. Librares loaded.")
 
 # ------------------------------------------------------------------------------
 ### Loading the dataset
-cli = rast("/home/jamesash/climate/data/cli_mon_sum_20240118.nc")
+cli = rast("/home/jamesash/climate/data/cli_day_20240110.nc")
 
 print("3. Data loaded")
 
 # ------------------------------------------------------------------------------
-### Plotting
+### Plotting prep.
 # I may need to extract values here. I don't want to lower the suprimum.
- 
-o = sd(values(cli[[2]]), na.rm = TRUE)
-l = min(values(cli[[2]]), na.rm = TRUE)
-u = max(values(cli[[2]]), na.rm = TRUE)
+cli = cli[[2]]
 
-infi = l # + o*2
-supi = u - o*2
+o = sd(values(cli), na.rm = TRUE)
+l = min(values(cli), na.rm = TRUE) 
+u = max(values(cli), na.rm = TRUE) 
+infi = l 
+supi = u - o
 
-cli = clamp(cli[[2]], lower=infi, upper=supi, values=TRUE)
+cli = clamp(cli, lower=infi, upper=supi, values=TRUE)
+
 print("5. Clamped.")
 
 # -------------------------------------------------------------------------------
-wdmap <- getMap(resolution = "high")
+### Plotting
+wdmap  = getMap(resolution = "high")
 colmap = cmocean("delta")(100)
 dt = gsub("-", "", as.character(Sys.Date()))
-e = ext(cli[[2]])
+e  = ext(cli)
 
 # Plotting the function. 
-pdf(paste("/home/jamesash/climate/figures/", "cli_mon_2_", dt, ".pdf", sep = ""),  
-    width = 5.5, # inches
+pdf(paste("/home/jamesash/climate/figures/", "cli_day_", dt, ".pdf", sep = ""),  
+    width  = 5.5, # inches
     height = 4,
     pointsize = 10) # inches
 
 plot(cli, 
 	ylim = c(15, 40),
 	xlim = c(-175, -130),
-	col = colmap, 
-	mar = c(3.1, 3.1, 2.1, 7.1),
-	plg = list(size = c(1, 1.25)),
-	range = c(-0.0007, 0.0007),
+	col  = colmap, 
+	mar  = c(3.1, 3.1, 2.1, 7.1),
+	plg  = list(size = c(1, 1.25)),
+	range = c(-0.0008, 0.0008),
 	ylab = "Latitude",
 	xlab = "Longitude")
 	#breaks = 100)
 plot(wdmap,
      ylim = e[3:4],
      xlim = e[1:2],
-     asp = 1,
-     bg = "black",
+     asp  = 1,
+     bg   = "black",
      border = "black",
-     col = "black",
-     add = TRUE,
-     lwd = 1)
+     col  = "black",
+     add  = TRUE,
+     lwd  = 1)
 dev.off()
 
 # ------------------------------------------------------------------------------
