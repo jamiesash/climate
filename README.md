a9e56a0b0773df0dfafbdcb4e637d642bc1d076a
James Ash <urzasmine@gmail.com>
2024-02-16 18:08:43 -1000

    made a nice figure finnaly

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # climate
-making climate maps of chl on the HPC. 
+making climate maps of chl on the HPC.
+
+I needed to download a fortran app. to install many of the packages required for this project.
+On my home computer all package and app installations where done under the "base" conda env. 
+ 
--- code/rcode/chl_fig_mon.R
+++ code/rcode/chl_fig_mon.R
@@ -3,78 +3,126 @@
 library(cmocean)
 library(anytime)
 library(terra)
+# for image.plot colorbar
+library(fields) 
 
 print("2. Librares loaded.")
 
 # ------------------------------------------------------------------------------
 ### Loading the dataset
 states = vect("../../data/states/s_05mr24.shp")
+idx = states[[c(1)]] == "HI"
+hawaii = states[idx]
+
 cli = rast("../../data/cli_mon_sum_20240118.nc")
 cli = cli[[2]]
+# per year not month
+cli = cli*12
 
 print("3. Data loaded")
+
 # ------------------------------------------------------------------------------
-### Plotting
-# I may need to extract values here. I don't want to lower the suprimum.
-
-# for dayly I use c(-0.00005, 0.00005)
-bounds = c(-0.001, 0.001)
-cli = clamp(cli, lower=bounds[1], upper=bounds[2], values=TRUE)
-print("5. Clamped.")
-
-# -------------------------------------------------------------------------------
 colmap = cmocean("delta")(100)
-dt = gsub("-", "", as.character(Sys.Date()))
-e = ext(cli)
+zmap = cmocean("delta")(30)
+
+# Truncating lower bounds.
+bounds = c(-0.02, 0)
+lcli = clamp(cli, lower=bounds[1], upper=bounds[2], values=TRUE)
+
+# Truncating upper bounds
+bounds = c(0, 0.005)
+ucli = clamp(cli, lower=bounds[1], upper=bounds[2], values=FALSE)
+bounds = c(0.005, 1)
+sucli = clamp(cli, lower=bounds[1], upper=bounds[2], values=FALSE)
 
 # Plotting the function. 
 pdf(paste("../../figures/", "cli_mon_", dt, ".pdf", sep = ""),  
-    width = 5.5, # inches
+    width = 6.5, # inches
     height = 4,
-    pointsize = 8) # inches
+    pointsize = 7) # inches
 
-image(cli,
-      col = colmap,
-      ylim = c(16, 40),
-      #zlim = c(-0.00005, 0.00005),
-      xlim = c(-175, -130),
-      asp = 0.83,
+# Note: asp will fix the white box issue but it comes at a price of 
+# messing with the lat lon ratio.
+par(mar=c(4, 4, 3, 7))
+image(lcli,
+      col = colmap[1:50],
+      ylim = c(16, 39),
+      xlim = c(-175, -132),
       axes = FALSE)
-plot(states,
+image(slcli,
+      col = colmap[1:3],
+      ylim = c(16, 40),
+      xlim = c(-175, -132),
+      add = TRUE,
+      axes = FALSE)
+image(ucli,
+      col = colmap[50:100],
+      ylim = c(16, 40),
+      xlim = c(-175, -132),
+      add = TRUE,
+      axes = FALSE)
+image(sucli,
+      col = colmap[90:100],
+      ylim = c(16, 39),
+      xlim = c(-175, -130),
+      add = TRUE,
+      axes = FALSE)
+plot(hawaii,
      col = "black",
-     # asp = 1,
      add = TRUE)
 axis(side = 2, 
      las = 2, 
-     lwd = 1, 
+     lwd = 0,
+     lwd.ticks = 1,
      at = c(18, 22, 26, 30, 34, 38),
      mgp = c(1, 0.75, 0), 
      cex.axis = 1)
 axis(side = 1, 
      las = 1, 
-     lwd = 1,
+     lwd = 0,
+     lwd.ticks = 1,
      mgp = c(2, 1, 0),    
-     at = c(-170, -160, -150, -140, -130),
+     at = c(-173, -168, -163, -158, -153, -148, -143, -138, -133),
      cex.axis = 1)
-box(lwd = 1)
+title(expression(Climate~Signal~of~Summertime~Chlorophyll~from~1997~to~2023~(mg~m^-3~year^-1)))
+title(ylab = "Latitude", line = 2)
+title(xlab = "Longitude", line = 2)
+image.plot(zlim = c(0, 0.005),
+           smallplot= c(0.89, 0.93, 0.52, 0.91),
+           axis.args = c(lwd = 0, lwd.ticks = 0.5),
+           col = zmap[15:30],
+           nlevel = 10,
+           add = TRUE, 
+           legend.only = TRUE)
+image.plot(zlim = c(-0.02,0),
+           axis.args = c(lwd = 0, lwd.ticks = 0.5),
+           smallplot= c(0.89, 0.93, 0.12, 0.51),
+           col = zmap[1:15],
+           nlevel = 10,
+           add = TRUE, 
+           legend.only = TRUE)
+box(lwd = 1, col = "black")
+
 dev.off()
 
+# getting frustrated and starting my own color bar.
+# A = matrix(nrow = 1,
+#            ncol = 50,
+#            data = 1:50) 
+# 
+# image(A,
+#       col = zmap,
+#         axes = FALSE)
+# axis(side = 4, 
+#      las = 1, 
+#      lwd = 0,
+#      lwd.ticks = 1,
+#      mgp = c(2, 1, 0),    
+#      cex.axis = 1)
+# box(lwd = 1, col = "black")
 
-pdf(paste("../../figures/", "cli_mon_", dt, ".pdf", sep = ""),  
-    width = 5.5, # inches
-    height = 4,
-    pointsize = 8) # inches
-plot(cli, 
-     col = colmap,
-     range=c(-0.001, 0.001),
-     mar=c(4, 4, 4, 6), 
-     #pal=list(shrink=0.9, cex=.8), 
-     pax=list(cex.axis=1.25))
-plot(states,
-     col = "black",
-     add = TRUE)
-dev.off()
 
 
-# ------------------------------------------------------------------------------
+
+
 
