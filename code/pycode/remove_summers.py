beaca27d7242d58456925c45fb0e4d375cdff4fd
jamiesash <jamesash@hawaii.edu>
2026-03-31 15:57:33 -1000

    summer months remove script

---
+++ code/pycode/remove_summers.py
@@ -0,0 +1,70 @@
+import numpy as np
+from netCDF4 import Dataset
+
+# -- Load cropped and masked anomaly data --
+file_id = Dataset('/home/jamesash/koa_scratch/chl_anomaly_cropped_masked_20260328.nc')
+ras  = file_id.variables["CHL_anom"][:].filled(np.nan).astype('float64')
+lat  = file_id.variables["latitude"][:].copy()
+lon  = file_id.variables["longitude"][:].copy()
+time = file_id.variables["time"][:].copy()
+time_units    = file_id.variables["time"].units
+time_calendar = getattr(file_id.variables["time"], 'calendar', 'standard')
+cz_mask = file_id.variables["CZ_mask"][:].astype(bool)
+file_id.close()
+
+ras[ras > 50] = np.nan
+
+# -- Build date vector --
+timedelta_vector = (time * np.timedelta64(1, 'D')).astype('timedelta64[ns]')
+base_date   = np.datetime64('1900-01-01')
+date_vector = base_date + timedelta_vector
+
+# -- Keep only summer months: June (6) through October (10) --
+months = date_vector.astype('datetime64[M]').astype(int) % 12 + 1
+summer_mask = (months >= 6) & (months <= 10)
+
+ras_summer  = ras[summer_mask, :, :]
+time_summer = time[summer_mask]
+date_summer = date_vector[summer_mask]
+
+print(f'Total time steps: {len(date_vector)}')
+print(f'Summer time steps: {len(date_summer)}')
+print(f'First: {str(date_summer[0])[:10]}, Last: {str(date_summer[-1])[:10]}')
+
+# -- Save summer-only anomaly as NetCDF --
+out_path = '/home/jamesash/koa_scratch/chl_anomaly_summer_20260328.nc'
+out = Dataset(out_path, 'w', format='NETCDF4')
+
+out.createDimension('time', ras_summer.shape[0])
+out.createDimension('latitude', len(lat))
+out.createDimension('longitude', len(lon))
+
+time_var = out.createVariable('time', 'f8', ('time',))
+time_var[:] = time_summer
+time_var.units = time_units
+time_var.calendar = time_calendar
+
+lat_var = out.createVariable('latitude', 'f4', ('latitude',))
+lat_var[:] = lat
+lat_var.units = 'degrees_north'
+
+lon_var = out.createVariable('longitude', 'f4', ('longitude',))
+lon_var[:] = lon
+lon_var.units = 'degrees_east'
+
+anom_var = out.createVariable('CHL_anom', 'f4', ('time', 'latitude', 'longitude'),
+                              fill_value=np.nan, zlib=True)
+anom_var[:] = ras_summer
+anom_var.units = 'mg m^-3'
+anom_var.long_name = 'Chlorophyll anomaly, summer only (Jun-Oct)'
+
+mask_var = out.createVariable('CZ_mask', 'i1', ('latitude', 'longitude'), zlib=True)
+mask_var[:] = cz_mask.astype(np.int8)
+mask_var.long_name = 'Contiguous zone mask (1 = inside CZ, 0 = open ocean)'
+
+out.description = 'CHL anomaly, cropped, CZ masked, summer months only (Jun-Oct)'
+out.source_file = 'chl_anomaly_cropped_masked_20260328.nc'
+out.history = f'Created {np.datetime64("today")}'
+
+out.close()
+print(f'Saved: {out_path}')
